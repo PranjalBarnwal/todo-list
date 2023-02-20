4e86fd641034edabc01e37ba08d70a43ee09e944
Pranjal <happybarnwal007@gmail.com>
2023-02-20 19:35:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//  Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+//Functions
+function deleteCheck(e) {
+  const item = e.target;
+  //DELETE TODO
+  if (item.classList[0] == "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+  //Check Mark
+  if (item.classList[0] == "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function addTodo(event) {
+  //preventing form from submitting
+  event.preventDefault();
+
+  //checking if input string is empty
+  if (todoInput.value == "") {
+   return;
+  }
+
+  //creating todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //creating  li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //creating CheckMarked button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fa fa-check" ></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //creating Delete button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fa fa-trash" ></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //Appending todoDiv to todoList
+  todoList.appendChild(todoDiv);
+
+  //clear todoInput value
+  todoInput.value = "";
+}
